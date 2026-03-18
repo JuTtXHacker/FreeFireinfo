633dd54f2e6607b9ed5918d4434bd7e582c374fa
MatricFailHacker <makjutt2233@gmail.com>
2026-03-18 09:50:26 +0500

    Add files via upload

--- Proto/build.sh
+++ Proto/build.sh
@@ -4,7 +4,7 @@ mkdir -p compiled
 cd "$(dirname "$0")"
 
 # Compile all .proto files into ./compiled/
-protoc -I=. --python_out=./compiled *.proto
+/usr/bin/protoc -I=. --python_out=./compiled --experimental_allow_proto3_optional *.proto
 
 # Fix imports to use package-style paths
 sed -i 's/^import \(.*_pb2\)/from Proto.compiled import \1/' compiled/*.py
